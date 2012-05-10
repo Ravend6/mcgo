f166ada37100d223b5ecd4b28fae028521d9f5ad
Jannis Friedmann <jannis.friedmann@telamitto.org>
2012-05-11 00:37:49 +0200

    Add open window on startup
    * init glfw and open window with 32bit color
    * add commandline-option to set window size and toggle fullscreen

---
+++ window.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jteeuwen/glfw"
+	"flag"
+	"time"
+)
+
+var width = flag.Int("width", 800, "width of the window")
+var height = flag.Int("height", 600, "height of the window")
+var fullscreen = flag.Bool("fullscreen", false, "enable fullscreen")
+
+func main() {
+	fmt.Println("starting")
+	flag.Parse()
+	
+	var err error
+	
+	err = glfw.Init()
+	if err != nil {
+		fmt.Printf("glfw.Init: %s", err)
+		return;
+	}
+	
+	windowMode := glfw.Windowed
+	if *fullscreen {
+		windowMode = glfw.Fullscreen
+	}
+	err = glfw.OpenWindow(*width, *height, 8, 8, 8, 8, 0, 0, windowMode)
+	if err != nil {
+		fmt.Printf("glfw.OpenWindow: %s", err)
+		return;
+	}
+	
+	time.Sleep(5 * time.Second)
+	
+	fmt.Println("the end")
+}
+
